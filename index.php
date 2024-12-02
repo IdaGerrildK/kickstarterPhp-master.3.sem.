fe77a3c06d8dd3e0d3abd05ba828af004c88e82a
IdaGerrild <imck43541@edu.ucl.dk>
2024-12-02 11:13:57 +0100

    tilføjet farver, cards mm.

--- index.php
+++ index.php
@@ -4,54 +4,196 @@ require "settings/init.php";
 <!DOCTYPE html>
 <html lang="da">
 <head>
-	<meta charset="utf-8">
-	
-	<title>Sigende titel</title>
+    <meta charset="utf-8">
 
-    <h1>yolo</h1>
-	
-	<meta name="robots" content="All">
-	<meta name="author" content="Udgiver">
-	<meta name="copyright" content="Information om copyright">
-	
-	<link href="css/styles.css" rel="stylesheet" type="text/css">
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mad for grønskollinger</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
 </head>
-
 <body>
-<div class="row g-2">
-	<?php
-	$produkter = $db->sql("SELECT * FROM produkter");
-	foreach($produkter as $produkt) {
-		?>
-		<div class="col-12 col-md-6">
-			<div class="card w-100">
-				<div class="card-header">
-					<?php
-					echo $produkt->prodNavn;
-					?>
-				</div>
-				<div class="card-body">
-					<?php
-					// Indsæt andet felt fra database
-					?>
-				</div>
-				<div class="card-footer text-muted">
-					<?php
-					// Indsæt andet felt fra database
-					?>
-				</div>
-			</div>
-		</div>
-		<?php
-	}
-	?>
+<div class="bg-light py-2 text-center">
+    <a class="navbar-brand" href="index.html"><img class="logo" src="img/fuldlogogroenskrift.png" alt="logo"></a>
 </div>
-<h1>hej</h1>
+<nav class="navbar navbar-expand-lg bg-secondary z-1 sticky-top">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Måltider
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="nav-item w-100">
+                            <a class="btn btn-outline-success w-100" href="morgenmad.html" role="button">Morgenmad</a>
+                        </li>
+                        <li class="nav-item w-100">
+                            <a class="btn btn-outline-success w-100" href="morgenmad.html" role="button">Frokost</a>
+                        </li>
+                        <li class="nav-item w-100">
+                            <a class="btn btn-outline-success w-100" href="morgenmad.html" role="button">Aftensmad</a>
+                        </li>
+                        <li class="nav-item w-100">
+                            <a class="btn btn-outline-success w-100" href="morgenmad.html" role="button">Dessert</a>
+                        </li>
+                        <li class="nav-item d-md-none">
+                        </li>
+                    </ul>
 
-<h1>hej</h1>
+                <li class="nav-item">
+                    <a class="nav-link" href="omforeningen.html">Om foreningen</a>
+                </li>
 
+                <li class="nav-item">
+                    <a class="nav-link" href="minkonto.html">Min konto</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true"></a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Søg efter opskrift her..." aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Søg</button>
+            </form>
+        </div>
+    </div>
+</nav>
+<br>
+<br>
+<h2 class="text-success text-center d-none d-lg-block">Ep. 8: Influencers mod Masterchef-deltagere:</h2>
+<h1 class="text-warning text-fluid text-center d-none d-lg-block text-bg-success p-3">Hvem laver den bedste (og mest kreative) veganske peberkage?</h1>
+<a href="https://www.youtube.com/">
+    <img src="img/mortenmasterchef3.png" class="img-fluid" alt="Youtube video med Morten, Naja, Gustav og Naja">
+</a>
+<br>
+<br>
+<br>
+<h1 class="text-success text-fluid text-center d-none d-lg-block text-bg-secondary p-3">Mød vores Grønskolling-repræsentanter</h1>
+<div class="row g-4">
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card">
+            <img src="img/morten.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="card-title text-center">Morten</h2>
+                <p class="card-text text-center">Influencer</p>
+                <div class="text-center">
+                    <a><button type="button" class="btn btn-outline-success">Læs mere</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card">
+            <img src="img/laura.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="card-title text-center">Laura</h2>
+                <p class="card-text text-center">Deltager fra Masterchef</p>
+                <div class="text-center">
+                    <a><button type="button" class="btn btn-outline-success button-center">Læs mere</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card">
+            <img src="img/naja.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="card-title text-center">Naja</h2>
+                <p class="card-text text-center">Influencer</p>
+                <div class="text-center">
+                    <a><button type="button" class="btn btn-outline-success">Læs mere</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="card">
+            <img src="img/gustav.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h2 class="card-title text-center">Gustav</h2>
+                <p class="card-text text-center">Vinder af Masterchef - De Unge Talenter</p>
+                <div class="text-center">
+                    <a><button type="button" class="btn btn-outline-success">Læs mere</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card mb-3">
+    <img class="card-img-bottom" src="img/teenagerscooking.jpg" alt="Card image cap">
+    <div class="card-body">
+        <div class="text-center">
+            <h3 class="card-title">Alle skal have muligheden for at lære at lave god mad!
+                For dig, dyrene og miljøets skyld!</h3>
+            <br>
+            <a href="omforeningen.html">
+                <button type="button" class="btn btn-outline-success btn-lg" role="button">Lær mere om vores værdier her</button></a>
+            <br>
+            <br>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<a href="index.html">
+    <div class="d-flex flex-column align-items-center text-center">
+        <img src="img/logo150.png" alt="Profil billede" class="img-fluid rounded-circle" width="150">
+        <div class="mt-3">
+        </div>
+    </div>
+</a>
+<br>
+<div class="row g-4">
+    <div class="col-6 col-md-4 col-lg-3">
+        <div class="text-dark-emphasis container-fluid text-center text-bg-light">
+            <div class="card mb-3 card-bg" style="max-width: 540px;">
+                <img src="img/mortenmobil.png" class="img-fluid rounded-start" alt="Mortenpåtiktok">
+                <div class="card-body">
+                    <h5 class="card-title">Følg os på de sociale medier!</h5>
+                    <br>
+                    <p class="card-text">Hvorfor ikke have muligheden for at have os med på farten, når du handler ind til dit næste Grønskolling-mesterværk? Eller i køkkenet, når du er igang? Du vil da heller ikke gå glip af vores mange konkurrencer heller?
+                    <div class="row g-4">
+                        <div class="col-6 col-md-4 col-lg-3">
+                            <div class="d-flex justify-content-center align-items-center flex-wrap">
+                                <a href="https://www.tiktok.com">
+                                    <img src="img/tiktoklogotegnet.png" class="img-fluid rounded-start" alt="tiktok link">
+                                    <span><a class="text-success" href="https://tiktok.com">Tiktok</a></span>
+                                </a>
+                                <br>
+                                <br>
+                                <div class="d-flex justify-content-center align-items-center flex-wrap">
+                                    <a href="https://www.instagram.com">
+                                        <img src="img/instalogotegning.png" class="img-fluid rounded-" alt="instagram link">
+                                        <span><a class="text-success" href="https://www.instagram.com">Instagram</a></span>
+                                    </a>
+                                </div>
+                                <br>
+                                <div class="d-flex justify-content-center align-items-center flex-wrap">
+                                    <a href="https://www.facebook.com">
+                                        <img src="img/facebooklogotegning.png" class="img-fluid rounded-start" alt="facebook link">
+                                        <span><a class="text-success" href="https://www.facebook.com">Facebook</a></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- settings/init.php
+++ settings/init.php
@@ -31,7 +31,7 @@ define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment || 2: Do
 
 if(CONFIG_LIVE == 0) {
     $DB_SERVER = "localhost";
-    $DB_NAME = "test";
+    $DB_NAME = "";
     $DB_USER = "root";
     $DB_PASS = "root";
 } else if(CONFIG_LIVE == 1) {
