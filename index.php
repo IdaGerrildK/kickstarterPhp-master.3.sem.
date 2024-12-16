3dc753593de713a106741c519d0ea31098135214
JulieAmalie <jur003@edu.zealand.dk>
2024-12-16 14:08:49 +0100

    tiføjet billede igen

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ require "settings/init.php";
 <br>
 <br>
 
-<img src="img/infografikigen.webp" class="img-fluid d-flex justify-content-center align-items-center" width="425"
+<img src="img/infografikigen.webp" class="img-fluid d-block mx-auto"
 <br>
 <h2 class="text-success text-center d-none d-lg-block">Ep. 3 > I samarbejde med NATURLI:
     Influencers springer ud i det grønne:</h2>
